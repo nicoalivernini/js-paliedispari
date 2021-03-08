230a95974f3ee2f9876c771fef3a7bb5d4245447
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-08 15:16:14 +0100

    Creazione funzione - Palindroma

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
-console.log('ok');
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+
+// Chiediamo e l'utente inserisce la parola
+var string = prompt('Inserisci una parola');
+
+// Creiamo la funzione per capire se la parola è palindroma
+
+function checkPalindroma () {
+  //.split separa la parola e inserisce ogni lettera in un cassetto dell'array
+  //.reverse inverte l'ordinamento dell'array
+  //.join riunisce tutti gli elementi dell'array in una stringa
+  return () == ().split('').reverse().join('');
+}
+console.log();
